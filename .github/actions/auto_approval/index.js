105f2170aa99018ddb6a4c0d1b13de704b7a29a2
Menny Even Danan <menny@evendanan.net>
2022-12-18 16:05:25 -0500

    Initial impl

---
+++ .github/actions/auto_approval/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const main = async () => {
+  const token = core.getInput('token')
+  const context = github.context;
+  const octokit = github.getOctokit(token)
+
+  await octokit.pulls.createReview({
+    ...context.repo,
+    pull_number: context.event.number,
+    event: 'APPROVE'
+  })
+}
+
+main().catch(err => setFailed(err.message))
